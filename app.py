b31510c836d80503f52b394822e40fb1c760ced9
masinde sylvester <masinde.sylvester@yahoo.com>
2025-01-22 23:00:53 +0300

    initial commit

--- app.py
+++ app.py
@@ -14,13 +14,13 @@ class Safaricom_employees(db.Model):
     __tablename__ = 'employees'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, nullable = False)
+    name = db.Column(db.String, nullable=False)
     department = db.Column(db.String, nullable=False)
     salary = db.Column(db.Float, nullable=False)
     job_group = db.Column(db.String, nullable=False)
     branch = db.Column(db.String, nullable=False)   
     employer_id = db.Column(db.Integer, db.ForeignKey('employer.id'),nullable=False) 
-    employer = db.relationship('Safaricom')
+    employer = db.relationship('Safaricom', backref = 'employer')
 
     def __repr__(self):
         return f'employee name: {self.name}, department: {self.department}, salary: {self.salary}, job_group: {self.job_group}, branch: {self.branch}'
